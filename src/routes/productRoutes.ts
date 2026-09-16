a8cc6e5ac1cfe0bfe4c582d7af5639827f7253cb
Philip's fedora wsl on windows <philipmartinantolihao@gmail.com>
2026-09-16 19:47:09 +0800

    fix/improper-productRoutes

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -15,4 +15,7 @@ router.post("/", createProduct);
 router.get("/", getProducts);
 router.get("/:id", getProduct);
 router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.delete("/:id", deleteProduct); 
+
+export default router; 
+
